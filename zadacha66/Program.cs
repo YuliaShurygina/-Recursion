243ef340eb95b32669f08141c87d04d3368d4065
Yulia Shurygina <zavalishina-yu@mail.ru>
2022-01-25 18:53:45 +0300

    Print numbers from 1 to N

---
+++ zadacha66/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать натуральные числа от 1 до N, N задано
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()??"0");
+void PrintNumbers(int number, int maximum)
+{
+    Console.Write($"{number}  ");
+    number++;
+    if (number <= maximum)
+        PrintNumbers(number, maximum);
+}
+PrintNumbers(1, N);
