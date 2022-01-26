123493f6d8efce1ed410434f948aedc9b8e98743
Yulia Shurygina <zavalishina-yu@mail.ru>
2022-01-26 21:50:34 +0300

    Words from 4 letters

---
+++ zadacha74/Program.cs
@@ -0,0 +1,19 @@
+﻿// В некотором машинном алфавите имеются четыре буквы «а», «и», «с» и «в». Покажите все слова, состоящие из n букв, которые можно построить из букв этого алфавита
+//char[] = {'а', 'и', 'с', 'в'};
+int n = 1;
+void FindWords(string alphabet, char[] word, int length = 0)
+{
+    if (length == word.Length)
+    {
+        Console.WriteLine($"{n++} {new string(word)}"); return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[length] = alphabet[i];
+        FindWords(alphabet, word, length + 1);
+    }
+}
+string letters = "аисв";
+char [] words = new char[3];
+FindWords(letters,  words);
+
