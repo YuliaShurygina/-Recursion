b38229aae63aa65ba0cd0c91b89b208fd70e9bfd
Yulia Shurygina <zavalishina-yu@mail.ru>
2022-01-26 16:08:03 +0300

    Find sum of digits of some number

---
+++ zadacha70/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти сумму цифр числа
+int SumOfDigits(int number)
+{
+    if (number == 0) return  0;
+    else return number % 10 + SumOfDigits(number / 10);
+}
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()??"0");
+int sumOfDigits = SumOfDigits(n);
+Console.WriteLine($"Сумма цифр числа равна {sumOfDigits} ");
