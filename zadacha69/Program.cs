30dff45289ce69c98d1eca5d11c98aafcd8a9c54
Yulia Shurygina <zavalishina-yu@mail.ru>
2022-01-26 15:22:09 +0300

    Find sum of  numbers from M  to N

---
+++ zadacha69/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+int SumOfNumbers(int n, int m)
+{
+    if (n == m) return m;
+    else   return n + SumOfNumbers(n-1, m);
+}
+Console.Write(" Введите число M: ");
+int M = int.Parse(Console.ReadLine()??"0");
+Console.Write(" Введите число N: ");
+int N = int.Parse(Console.ReadLine()??"0");
+int sumOfNumbers = SumOfNumbers(N, M);
+Console.WriteLine($"Сумма элементов от {M} до {N} равна {sumOfNumbers}");
