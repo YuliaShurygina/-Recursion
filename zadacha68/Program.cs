b1ff66e505da9ab10c8c4288faf6589bc63c2793
Yulia Shurygina <zavalishina-yu@mail.ru>
2022-01-26 14:25:57 +0300

    Print numbers from M  to N

---
+++ zadacha68/Program.cs
@@ -0,0 +1,21 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+void PrintNumbersRight(int m, int n)
+{
+    Console.Write($"{m} ");
+    m++;
+    if (m <= n)
+       PrintNumbersRight(m, n);
+}
+void PrintNumbersLeft(int m, int n)
+{
+    Console.Write($"{m} ");
+    m--;
+    if (m >= n)
+       PrintNumbersLeft(m, n);
+}
+Console.Write(" Введите число M: ");
+int M = int.Parse(Console.ReadLine()??"0");
+Console.Write(" Введите число N: ");
+int N = int.Parse(Console.ReadLine()??"0");
+if (N> M) PrintNumbersRight(M, N);
+else PrintNumbersLeft(M, N);
