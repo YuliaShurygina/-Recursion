d6687efa1e737d3a69e92b180b62ba6b7a8b315b
Yulia Shurygina <zavalishina-yu@mail.ru>
2022-01-26 18:52:40 +0300

    Print the first n elements, which each next number equal of two previous elements sum

---
+++ zadacha73/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. Первые два элемента последовательности задаются пользователем
+double Fibonacci(int number, int A, int B)
+{
+    if (number == 1 ) return A;
+    else if (number == 2) return B;
+    else return Fibonacci(number-1, A, B)+ Fibonacci(number -2, A, B);
+}
+
+Console.Write("Введите количество элементов последовательности: ");
+int n = int.Parse(Console.ReadLine()??"0");
+Console.Write("Введите первый элемент последовательности: ");
+int a = int.Parse(Console.ReadLine()??"0");
+Console.Write("Введите второй элемент последовательности: ");
+int b = int.Parse(Console.ReadLine()??"0");
+for (int i = 1; i <= n; i++)
+    {
+        Console.WriteLine($"f({i}) = {Fibonacci(i, a, b)} ");
+    }
